bb1135e83fa83c4d7e51289c6b68eab039ede449
Cristian Kesit <cristian.kesit@ti.ukdw.ac.id>
2022-10-24 08:41:59 +0700

    commit

---
+++ src/main/java/org/example/Vehicle.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public interface Vehicle {
+    public void backward();
+    public void brake();
+    public void forward();
+    public void turnLeft();
+    public void turnRight();
+}
---
+++ src/main/java/org/example/Wheel.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Wheel {
+    private int width;
+    private int diameter;
+
+    public Wheel(int width, int diameter){
+        this.width=width;
+        this.diameter=diameter;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public int getwidth() {
+        return width;
+    }
+
+
+}
